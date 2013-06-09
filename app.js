293be79be5a5658e00382a108151207304f67a04
vagrant <vagrant@precise32.(none)>
2013-06-09 07:48:36 +0000

    Fixed index rendering bug

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+
+/**
+ * Module dependencies.
+ */
+
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
 var app = express();
 
-app.set('view', __dirname + '/views');
-app.set('view engine', 'html');
-
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'ReCharge' }, function(err, html){
-  	console.log('Index rendering error.');
-  });
+  res.render('index', { title: 'ReCharge' });
 };
